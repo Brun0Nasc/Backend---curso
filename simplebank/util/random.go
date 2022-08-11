bebf4a7af1bb7aa9b9e7df1aa1d106c78a6a300f
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-08-11 11:48:37 -0300

    Add files via upload

---
+++ simplebank/util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt gera um integer aleatório entre o min e o max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner gera um nome de owner aleatório
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney gera uma quantidade de dinheiro aleatória
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
