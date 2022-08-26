5547af710ea505bbc642092ec19e5c13bdc176ce
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-08-26 11:44:51 -0300

    .

--- simplebank/db/sqlc/store_test.go
+++ simplebank/db/sqlc/store_test.go
@@ -33,6 +33,7 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// check result
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -73,7 +74,36 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		//TODO: check accounts' balance
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, fromAccount.ID, account1.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, toAccount.ID, account2.ID)
+
+		// check accounts balance
+		diff1 := account1.Balance - account2.Balance
+		diff2 := fromAccount.Balance - toAccount.Balance
+		require.Equal(t, account1, account2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff2 > 0)
+		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount, ...
+
+		k := int(diff1 / amount)
+		require.True(t, k>=1, k<=n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
 
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance - int64(n) * amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance + int64(n) * amount, updatedAccount2.Balance)
 }
